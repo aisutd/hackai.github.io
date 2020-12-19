5b17fd0e52f1e837938958e065ed3b8136c02de8
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-18 16:33:39 -0800

    Configure typescript, add Tailwind colors/font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        gunmetal: '#252b33',
+        oldlav: '#675d7b',
+        wisteria: '#c59ad5',
+        capri: '#11bbfd',
+      },
+    },
+    fontFamily: {
+      sans: `'IBM Plex Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
     },
   },
   variants: {
