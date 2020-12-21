acc50598fc7564a45ac41c9a8bca5bf4a27fd10c
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-20 19:44:29 -0800

    Adjust spacing in countdown

--- components/countdown.tsx
+++ components/countdown.tsx
@@ -3,7 +3,7 @@ import Count from 'react-countdown'
 
 const Countdown: FC = () => {
   return (
-    <div className='my-8 max-w-lg text-center text-2xl mx-auto px-6 py-8 bg-black bg-opacity-10 rounded-full select-none'>
+    <div className='my-8 max-w-lg text-center text-2xl mx-auto p-6 bg-black bg-opacity-10 rounded-full select-none'>
       <span>Applications open in </span>
       <Count date={1610172000000} />
     </div>
