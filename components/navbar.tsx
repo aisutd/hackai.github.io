83be49e21f81dec0d975879dd3ec9be67e574c36
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-24 00:34:03 -0800

    Hero and navbar aesthetic adjustments

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,9 +8,9 @@ interface NavbarItemProps {
 
 export const NavbarItem: FC<NavbarItemProps> = ({ text, href }: NavbarItemProps) => {
   const link = (
-    <a className='link cursor-pointer font-medium text-xl text-wisteria hover:text-gray-200 py-1 sm:py-1 md:py-auto lg:py-auto xl:py-auto'>
+    <div className='link text-center cursor-pointer font-medium text-xl text-wisteria hover:text-black hover:bg-wisteria px-1 py-3'>
       {text}
-    </a>
+    </div>
   )
   return href ? <Link href={href}>{link}</Link> : link
 }
@@ -19,10 +19,8 @@ const Navbar: FC = () => {
   return (
     <div
       id='navbar'
-      className='py-6 w-full flex items-center justify-center bg-black bg-opacity-10 mb-16'>
-      <div
-        className='w-full sm:w-full md:w-full lg:w-2/3 xl:w-3/10 flex flex-col sm:flex-col md:flex-row lg:flex-row xl:flex-row flex-wrap items-center justify-between px-8'
-        style={{ maxWidth: '800px' }}>
+      className='w-full flex items-center justify-center bg-black bg-opacity-20 mb-16'>
+      <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-4 xl:grid-cols-4 w-full max-w-3xl'>
         <NavbarItem text='Home' href='/' />
         <NavbarItem text='Interest Form' href='//form.typeform.com/to/NJCsokJY' />
         <NavbarItem text='Past Sponsors' href='/#past-sponsors' />
