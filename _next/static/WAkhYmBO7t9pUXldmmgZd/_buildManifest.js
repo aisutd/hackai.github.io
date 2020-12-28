77c4b607d2b3441fb0a5b967da898ffdcd6e0cae
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2020-12-27 18:24:25 -0800

    Build site from master @ eb0cc2cae3b46dc175aee15b6babbfa08faa7d1a

---
+++ _next/static/WAkhYmBO7t9pUXldmmgZd/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-11818f2c0894c7ba55e7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-53b965a9bed4bece3b78.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
