27ff34926c7fdcfbe909d3e59cfb1a0a81ecd4d9
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-04 11:56:06 -0800

    Remove redundant metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,36 +11,10 @@ import Countdown from '../components/countdown'
 import Attribution from '../components/attribution'
 
 const Home: FC = () => {
-  const eventMeta = `
-{
-  "@context": "https://schema.org",
-  "@type": "Event",
-  "name": "HackAI 2021",
-  "startDate": "2021-03-27T8:30:00-06:00",
-  "endDate": "2021-07-28T18:30-06:00",
-  "eventStatus": "https://schema.org/EventScheduled",
-  "eventAttendanceMode": "https://schema.org/OnlineEventAttendanceMode",
-  "location": {
-    "@type": "VirtualLocation",
-    "url": "https://hackai.org/"
-    },
-  "image": [
-    "https://hackai.org/assets/participants.jpg",
-    "https://hackai.org/assets/sponsors.jpg",
-   ],
-  "description": "A student-run annual hackathon aiming to connect today's students with the knowledge and resources needed to build AI-related projects in the span of 24 hours.",
-  "organizer": {
-    "@type": "Organization",
-    "name": "The Artificial Intelligence Society at UT Dallas",
-    "url": "https://aisutd.org/"
-  }
-}
-`
   return (
     <div>
       <Head>
         <title>HackAI: The Largest AI Hackathon at UT Dallas</title>
-        <script type='application/ld+json'>{eventMeta}</script>
       </Head>
       <Countdown />
       {/* <Attribution /> */}
