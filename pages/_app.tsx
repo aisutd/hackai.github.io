3596434b585730a4207b5ddeef5ac8d0a8860202
SaminKhan35 <61805312+SaminKhan35@users.noreply.github.com>
2021-01-04 12:13:40 -0600

    Update _app.tsx
    Added Next-SEO component for preview card

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,32 @@
 import { FC } from 'react'
 import type { AppProps } from 'next/app'
 import '../global.css'
+import { NextSeo } from 'next-seo'
+
 
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
+
+  <div>
+  <NextSeo
+      openGraph={{
+        type: 'website',
+        url: 'https://hackai.org/',
+        title: 'HackAI: The Largest AI Hackathon at Dallas',
+        description: 'HackAI is a student-run annual hackathon organized by the Artificial Intelligence Society at The University of Texas of Dallas, aiming to connect today\'s students with the knowledge and resources needed to build Artificial Intelligence (AI) related projects in the span of 24 hours.',
+        images: [
+          {
+            url: '/assets/hackai-logo.png',
+            width: 800,
+            height: 600,
+            alt: 'Og Image Alt',
+          }
+        ]
+      }}
+        
+    />
+  </div>
   return <Component {...pageProps} />
 }
 
 export default App
+
