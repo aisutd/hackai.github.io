78cf82cc4f0d85027994ff42249be9ab782ee299
Aditya Rathod <15635979+adityarathod@users.noreply.github.com>
2021-01-04 13:18:01 -0800

    Add OpenGraph image, update metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,29 +3,30 @@ import type { AppProps } from 'next/app'
 import '../global.css'
 import { NextSeo } from 'next-seo'
 
-
 const App: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-
   return (
-  <> 
-    <NextSeo
+    <>
+      <NextSeo
+        title='HackAI: The Largest AI Hackathon at Dallas'
+        description='HackAI is a student-run annual hackathon organized by the Artificial Intelligence Society at The University of Texas of Dallas.'
         openGraph={{
           type: 'website',
           url: 'https://hackai.org/',
           title: 'HackAI: The Largest AI Hackathon at Dallas',
-          description: 'HackAI is a student-run annual hackathon organized by the Artificial Intelligence Society at The University of Texas of Dallas, aiming to connect today\'s students with the knowledge and resources needed to build Artificial Intelligence (AI) related projects in the span of 24 hours.',
+          description:
+            "HackAI is a student-run annual hackathon organized by the Artificial Intelligence Society at The University of Texas of Dallas, aiming to connect today's students with the knowledge and resources needed to build Artificial Intelligence (AI) related projects in the span of 24 hours.",
           images: [
             {
-              url: '/assets/hackai-logo.png',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-            }
-          ]
+              url: '/assets/opengraph.png',
+              width: 1200,
+              height: 627,
+              alt: 'HackAI Logo',
+            },
+          ],
         }}
       />
-    <Component {...pageProps} />
-  </>
+      <Component {...pageProps} />
+    </>
   )
 }
 
