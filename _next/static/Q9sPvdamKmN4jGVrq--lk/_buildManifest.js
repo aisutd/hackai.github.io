9ec92e7e68b7c987bde77312b6ffd6bd3a4ddf3b
WillieCubed <dev@williecubed.me>
2021-03-12 03:04:39 +0000

    Automated build from Github Action

--- _next/static/Q9sPvdamKmN4jGVrq--lk/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-a661a34bf512cf0c9e46.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f6e72f6add08eb46f4da.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
