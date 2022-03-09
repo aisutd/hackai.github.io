14892f7a6aff7ba49c076c340cdbda231ddb2553
KumailB <sxb180119@utdallas.edu>
2022-03-09 12:29:07 -0600

    fix: team members limit from 5 to 4

--- faq.ts
+++ faq.ts
@@ -23,20 +23,23 @@ const faq: FAQ[] = [
   {
     q: 'How will team formation work?',
     a:
-      'Teams at HackAI can have a maximum of 5 members. If you already have team members in mind while signing up, you can include it in your application. Otherwise, you can form teams the day of the hackathon by meeting other participants in the Discord!',
+      'Teams at HackAI can have a maximum of 4 members. If you already have team members in mind while signing up, you can include it in your application. Otherwise, you can form teams the day of the hackathon by meeting other participants.',
   },
   {
     q: 'Do I need to have a lot of prior experience in AI?',
-    a: 'Not at all! The purpose of this hackathon is to introduce AI to everyone, so as a result you do not need experience with AI to attend. If you want to submit a project regardless of experience level, we have various "tracks" categorized by relative difficulty level by which projects submitted will be judged, and we will also have workshops to help beginners out. All you need is a drive and passion to learn!'
+    a:
+      'Not at all! The purpose of this hackathon is to introduce AI to everyone, so as a result you do not need experience with AI to attend. If you want to submit a project regardless of experience level, we have various "tracks" categorized by relative difficulty level by which projects submitted will be judged, and we will also have workshops to help beginners out. All you need is a drive and passion to learn!',
   },
   {
     q: 'What does "Where (A)I can do anything" mean?',
-    a: 'Any sort of program, be it a simple "Hello World" or something as complex as a walking, talking robot, will only ever really be as smart as its creator. That\'s why the "A" is in parentheses - because there\'s no "AI" without "I". The program can work because you can - and that\'s what we hope to accomplish at HackAI.'
+    a:
+      'Any sort of program, be it a simple "Hello World" or something as complex as a walking, talking robot, will only ever really be as smart as its creator. That\'s why the "A" is in parentheses - because there\'s no "AI" without "I". The program can work because you can - and that\'s what we hope to accomplish at HackAI.',
   },
   {
     q: 'What other events will be a part of HackAI?',
-    a: 'HackAI is a full day of learning about what the hype is behind AI - we have interactive workshops where you can make prototype projects with the host - and make them your own, so we\'ll guarantee that you\'ll come out of HackAI having learnt something new and having something to show for it. We also have talks with people who have worked in the AI field, so if you want to learn more, this is the best place to start! We\'re also proud that OpenAI, a company co-founded by Elon Musk, will be working with us in HackAI!'
-  }
+    a:
+      "HackAI is a full day of learning about what the hype is behind AI - we have interactive workshops where you can make prototype projects with the host - and make them your own, so we'll guarantee that you'll come out of HackAI having learnt something new and having something to show for it. We also have talks with people who have worked in the AI field, so if you want to learn more, this is the best place to start! We're also proud that OpenAI, a company co-founded by Elon Musk, will be working with us in HackAI!",
+  },
 ]
 
 export default faq
